9cb4ceaf15ea2fd04524d0b6adbf621cc8c51857
Mahmoud Abdelkader <MahmoudAbdelkader@domain.local>
2021-12-16 14:35:39 -0400

    Images uploaded plugins

--- config/database.js
+++ config/database.js
@@ -9,3 +9,39 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'ec2-52-54-38-229.compute-1.amazonaws.com'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'd8kqi9viki79a3'),
+//       user: env('DATABASE_USERNAME', 'qtbdvbhkxcskqm'),
+//       password: env('DATABASE_PASSWORD', '6f253835a4044da9d35d201e8f9893054e867a81a8e0990a108cb74ba47eca0e'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+
+   // strapi-api/config/database.js
+  //  module.exports = ({ env }) => ({
+  //   defaultConnection: "default",
+  //   connections: {
+  //     default: {
+  //       connector: "bookshelf",
+  //       settings: {
+  //         client: "postgres",
+  //         host: env('DATABASE_HOST', 'ec2-52-54-38-229.compute-1.amazonaws.com'),
+  //         port: env.int('DATABASE_PORT', 5432),
+  //         database: env('DATABASE_NAME', 'd8kqi9viki79a3'),
+  //         user: env('DATABASE_USERNAME', 'qtbdvbhkxcskqm'),
+  //         password: env('DATABASE_PASSWORD', '6f253835a4044da9d35d201e8f9893054e867a81a8e0990a108cb74ba47eca0e'),
+  //         schema: env("DATABASE_SCHEMA", "public"),
+  //       },
+  //       options: {},
+  //     },
+  //   },
+  // });
